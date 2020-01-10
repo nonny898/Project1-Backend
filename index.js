42e694c2842ab5b9114c4154613349aba7d671e6
Atichard Chintakanond <atichard.c@outlook.com>
2020-01-10 23:38:59 +0700

    show top ten words

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const cheerio = require('cheerio');
 const app = express();
 app.set('view engine', 'pug');
 
-let tallies = {};
-
 app.get('/', function(req, res) {
   res.render('index', { title: 'Home', message: 'Hello there!' });
 });
@@ -23,13 +21,46 @@ app.get('/wc', function(req, res) {
       const cleanArray = sparseArray.filter(function(el) {
         return el != '';
       });
-      console.log(cleanArray.length);
+      let total = 0;
+      let tallies = {};
       cleanArray.forEach((word) => {
         if (word.length < 20) {
+          total += 1;
           tallies[word] = tallies[word] ? tallies[word] + 1 : 1;
         }
       });
-      res.render('wordCount', { title: 'WordCount', number: cleanArray });
+      const frequencies = [];
+      Object.values(tallies).forEach((value) => {
+        frequencies.push(value);
+      });
+      const topTenFreq = frequencies
+        .sort((a, b) => {
+          return b - a;
+        })
+        .slice(0, 10);
+      const topTenWord = {};
+      Object.keys(tallies).forEach((values) => {
+        topTenFreq.forEach((freq) => {
+          if (tallies[values] === freq) {
+            topTenWord[values] = freq;
+            topTenFreq.splice(topTenFreq.indexOf(freq), 1);
+          }
+        });
+      });
+      const result = {};
+      Object.keys(topTenWord)
+        .sort((a, b) => {
+          return topTenWord[b] - topTenWord[a];
+        })
+        .forEach((key) => {
+          result[key] = topTenWord[key];
+        });
+      res.render('wordCount', {
+        title: 'WordCount',
+        url: url,
+        number: total,
+        result: result,
+      });
     } else {
       console.log(error);
     }
